09f3c758e116a76f36f63ea2eed2a7450624e915
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2022-05-23 22:42:19 +0200

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter as Router, Route } from 'react-router-dom'
+
+
+/*
+
+import Navbar from './components/Navbar'
+import Home from './pages/Home/Home';
+import SignUp from './pages/SignUp/SignUp';
+import Login from './pages/Login/Login'
+import Post from './pages/Post/Post';
+import Profile from './pages/Profile/Profile';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+    <>
+
+    <Navbar />
+    <Router>
+    
+     
+      <Route path="/" exact render={() => <Home /> } />
+      <Route path="/signup" exact render={() => <SignUp /> } />
+      <Route path="/login" exact render={() => <Login /> } />
+      <Route path="/post" exact render={() => <Post /> } />
+      <Route path="/profile" exact render={() => <Profile /> } />
+
+     
+    </Router>
+    </>
+
+   
+  )
+}
+
+export default App;
+
+*/
+
+import Home from './pages/Home/Home';
+import SignUp from './pages/SignUp/SignUp';
+import Login from './pages/Login/Login'
+import Profile from './pages/Profile/Profile'
+import Post from './pages/Post/Post'
+import Navbar from './components/Navbar'
+
+
+
+
+
+
+function App() {
+  return (
+  
+    <>
+
+    <Navbar />
+    <Router>
+    
+     
+      <Route path="/" exact render={(props) => <Home /> } />
+      <Route path="/signup" exact render={(props) => <SignUp /> } />
+      <Route path="/login" exact render={(props) => <Login /> } />
+      <Route path="/profile" exact render={(props) => <Profile /> } />
+      <Route path="/post" exact render={(props) => <Post /> } />
+    
+   
+   
+
+     
+    </Router>
+    </>
+
+   
+  )
 }
 
 export default App;
