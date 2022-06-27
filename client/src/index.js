624d4dfba2740efb25a5953285580ec12505c496
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2022-06-27 22:24:20 +0200

    delete import App.css

--- client/src/index.js
+++ client/src/index.js
@@ -51,7 +51,6 @@ import "./index.css";
 //import "./bootstrap.min.css";
 //import 'bootstrap/dist/css/bootstrap.css';
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -67,4 +66,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
