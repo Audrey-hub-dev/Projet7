b7835f69ddcf9f6e864b2e0d2f918ee95ca91b55
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2022-06-27 21:34:51 +0200

    delete comments

--- server/app.js
+++ server/app.js
@@ -3,31 +3,16 @@
  * par le frontend. 
  */
 
-
  const express = require('express');
 
  const app = express();
 
  const fs = require("fs");
 
-  //importation de path pour utiliser le dossier images
-  const path = require('path');
+//importation de path pour utiliser le dossier images
+const path = require('path');
  
-  
- 
- const cors = require("cors");
-/*
- const corsOptions = {
-  origin: ["http://localhost:3001"],
-  credentials: true,
-  'Access-Control-Allow-Credentials': 'true',
-  'allowedCredentials': 'true',
-  'allowedHeaders': ['sessionId', 'Content-Type'],
-  'exposedHeaders': ['sessionId'],
-  'methods': ['GET,HEAD,PUT,PATCH,POST,DELETE'],
-  'preflightContinue': true
-}
-*/
+const cors = require("cors");
 
  app.use(cors({
   credentials: true,
@@ -35,7 +20,6 @@
 })
 );
  
-//app.use(express.urlencoded({ extended: true }));
 
  const db = require("./models");
 
@@ -46,24 +30,6 @@
        console.log("Database connected !");
  });
 
-
- 
- /*
- //utilisation de cookie-session pour sécuriser les cookies de session 
- const cookieSession = require('cookie-session');
- 
- app.use(cookieSession({
-   name: 'session',
-   keys: [0],
-   // Cookie Options
-   maxAge: 24 * 60 * 60 * 1000, // 24 hours
-   secure: true,
-   httpOnly: true
- 
- }));
- */
-
- 
  app.use((req, res, next) => {
   //res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
@@ -72,39 +38,22 @@
 
   next();
 });
-
- //utilisation du module 'dotenv' pour masquer les informations de connexion à la base de données à l'aide de variables d'environnement
- //require('dotenv').config()
- //console.log(process.env)
  
  //pour analyser le corps de la requête 
  app.use(express.json());
  
-
  //permet de se servir du dossier images lors d'une requête 
  app.use('/images', express.static(path.join(__dirname, 'images'))); 
  
- //enregistrer les routes du fichier user.js, routes liées à l'authentification attendues par le frontend
-
-
-const { errorHandler, notFound } = require("./middlewares/error");
-
-
 const postRoutes = require("./routes/postRoutes");
 const userRoutes = require("./routes/userRoutes");
 const likesRoutes = require("./routes/Likes")
 const commentRoutes = require("./routes/Comments")
 
-
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/likes", likesRoutes);
 app.use("/api/comments", commentRoutes)
-
-
-// Error Handling middlewares
-app.use(notFound);
-app.use(errorHandler);
 
 
  module.exports = app; 
