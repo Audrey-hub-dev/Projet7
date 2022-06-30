0dd1274ecf3e931745a031a964376b8cef30b85e
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2022-06-30 15:39:17 +0200

    delete comments

--- client/src/index.js
+++ client/src/index.js
@@ -1,54 +1,6 @@
 
-/*
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-//import { applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from './pages/Reducers'
-//import thunk from 'redux-thunk';
-//import rootReducer from './reducers'
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './pages/Reducers/UserReducer';
-
-
-//import { editUser } from "./actions/user.actions"
-
-
-
-const store = configureStore ({
-  reducer: {
-    user: userReducer,
-  },  
-});
-
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-  <Provider store={store}>
- 
-    <App />
-  
-  </Provider>,
-
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-*/
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import "./bootstrap.min.css";
-//import 'bootstrap/dist/css/bootstrap.css';
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -62,6 +14,4 @@ root.render(
 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
